1104191d095a9660ade1d57758e52b3c87dbe670
shdlfgh63 <96168797+shdlfgh63@users.noreply.github.com>
2022-02-21 20:51:43 +0900

    Update Gmail.java

--- Lecture Evaluation/src/main/java/util/Gmail.java
+++ Lecture Evaluation/src/main/java/util/Gmail.java
@@ -5,6 +5,6 @@ public class Gmail extends Authenticator{
 
 	@Override
 	protected PasswordAuthentication getPasswordAuthentication() {
-		return new javax.mail.PasswordAuthentication("shdlfgh147","rih9592!@#" );
+		return new javax.mail.PasswordAuthentication("ID","Password" );
 	}
 }
