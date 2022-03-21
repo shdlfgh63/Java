11d764609284b9cbb035b4eccbadaa38d4b40b32
ilho Noh <shdlfgh63@naver.com>
2022-03-21 21:16:20 +0900

    스프링 시작

---
+++ SpringStudy/src/main/java/com/edu/exam/SampleController.java
@@ -0,0 +1,24 @@
+package com.edu.exam;
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam01")
+public class SampleController {
+    
+	private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+	@RequestMapping("/doA")
+	public void doA() {
+		logger.info("daA가 실행되었습니다");
+	}
+	
+	@RequestMapping("/doB")
+	public void doB() {
+		logger.info("daB가 실행되었습니다");
+	}
+}
---
+++ SpringStudy/src/main/webapp/WEB-INF/views/exam01/doB.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>doB.jsp</title>
+</head>
+<body>
+<h1>Hello doB</h1>
+ <p>doB 실행중</p>
+</body>
+</html>
