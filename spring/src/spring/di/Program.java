f44d63ab2b78986f35b532772e2197965081f666
ilho Noh <shdlfgh63@naver.com>
2022-03-23 20:40:15 +0900

    [update]
    DI 값형식, 생성자

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -3,6 +3,7 @@ package spring.di;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring.di.entity.Exam;
 import spring.di.ui.ExamConsole;
 
 public class Program {
@@ -19,6 +20,9 @@ public class Program {
 		
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		
 		ExamConsole console = (ExamConsole) context.getBean("console");
 		//ExamConsole console = context.getBean(ExamConsole.class);
--- spring/src/spring/di/entity/NewExam.java
+++ spring/src/spring/di/entity/NewExam.java
@@ -6,6 +6,49 @@ public class NewExam implements Exam {
     private int math;
     private int com;
     
+    public NewExam() {
+		
+	}
+    
+	public NewExam(int kor, int eng, int math, int com) {		
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
 	@Override
 	public int total() {
 		// TODO Auto-generated method stub
@@ -17,5 +60,12 @@ public class NewExam implements Exam {
 		// TODO Auto-generated method stub
 		return total() / 4.0f;
 	}
+
+	@Override
+	public String toString() {
+		return "NewExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	}
+	
+	
 
 }
