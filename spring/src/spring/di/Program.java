75f99d7e89b972b9a4ab3fa26c7f7457f0e32448
ilho Noh <shdlfgh63@naver.com>
2022-03-23 19:10:14 +0900

    [update]
    DI지시서 작성,maven

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -1,19 +1,28 @@
 package spring.di;
 
-import spring.di.entity.Exam;
-import spring.di.entity.NewExam;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.ui.ExamConsole;
-import spring.di.ui.GridExamConsole;
-import spring.di.ui.InlineExamConsole;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+	
+		/*	스프링에게 지시하는 방법으로 코드를 변경
 		Exam exam = new NewExam();
       //ExamConsole console = new InlineExamConsole(exam); //DI
-	    ExamConsole console = new GridExamConsole(exam);
-        console.print();
+	    ExamConsole console = new GridExamConsole();
+	 
+	    console.setExam(exam);
+        */
+		
+		ApplicationContext context = 
+				new ClassPathXmlApplicationContext("spring/di/setting.xml");
+		
+		ExamConsole console = (ExamConsole) context.getBean("console");
+		//ExamConsole console = context.getBean(ExamConsole.class);
+	    console.print();
 	}
 
 }
--- spring/src/spring/di/ui/GridExamConsole.java
+++ spring/src/spring/di/ui/GridExamConsole.java
@@ -4,7 +4,9 @@ import spring.di.entity.Exam;
 
 public class GridExamConsole implements ExamConsole {
 private Exam exam;
-	
+	public GridExamConsole() {
+		// TODO Auto-generated constructor stub
+	}
 	
 	public GridExamConsole(Exam exam) {		
 		this.exam = exam;
@@ -17,6 +19,11 @@ private Exam exam;
 		 System.out.printf("│   %3d   │  %3.2f   │\n",exam.total(),exam.avg());
 		System.out.println("└─────────┴─────────┘");
 
+	}
+	@Override
+	public void setExam(Exam exam) {
+		this.exam= exam;
+		
 	}
 
 }
